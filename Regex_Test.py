6580b1fb4a713f610a3bdb25bde29035e97c3184
Rafael Fagundes <65747985+0xrsfagundes@users.noreply.github.com>
2023-03-10 21:28:01 +0000

    Mar 10 2023

---
+++ Regex_Test.py
@@ -0,0 +1,17 @@
+import re
+
+def getAudienceID(str):
+
+    pattern = r'_d\w{10}-'
+    chars_to_remove = "_-"
+
+    match = re.search(pattern, str)
+    if match:
+        matched_string = match.group()
+        new_matched_string = matched_string
+
+        for char in chars_to_remove:
+            new_matched_string = new_matched_string.replace(char, "")
+    else:
+        return ""
+    return new_matched_string
