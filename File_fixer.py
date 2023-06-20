ecddd073264404f9adcd4aa8a79e2cd3166c4d2c
0xrsfagundes <rsfagundes@gmail.com>
2023-06-20 23:56:03 +0100

    Jun 20 2023

--- File_fixer.py
+++
@@ -1,7 +0,0 @@
-import pandas as pd
-
-df = pd.read_csv("C://Users//rafael_fagundes//OneDrive - Dell Technologies//Documents//1153793_Untitled_Report_20230309_160522_4048829742.csv", on_bad_lines='skip', skiprows=10)
-
-pd.set_option('display.max_columns', None)
-
-df.to_csv("C://Users//rafael_fagundes//OneDrive - Dell Technologies//Desktop//GCM.csv")
